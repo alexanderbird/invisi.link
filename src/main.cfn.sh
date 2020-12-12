e6806d63d78da952b673fdcf86989f8e591ad229
Alexander Bird <alex@alexanderbird.software>
2020-12-12 14:52:22 -0800

    Use jq's -f flag instead of cat for providing the jq input

--- src/main.cfn.sh
+++ src/main.cfn.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cat redirects | sed "s/\([a-z0-9]*\) *\(https*\):\/\/\(.*\)/{ \"slug\": \"\1\", \"protocol\": \"\2\", \"route\": \"\3\"}/" |
-  jq -s --argfile parameters $DIR/main.parameters.cfn.json "$(cat $DIR/main.cfn.jq)"
+  jq -s --argfile parameters $DIR/main.parameters.cfn.json -f $DIR/main.cfn.jq
